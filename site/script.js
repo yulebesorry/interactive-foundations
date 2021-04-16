f95436da436bb01e8c569e468e8882fab3530ab7
yulebesorry <58015079+yulebesorry@users.noreply.github.com>
2021-04-16 14:57:21 -0700

    fetch

---
+++ site/script.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+$(document).ready(function(){
+
+
+	//------------------------------------
+	// adds a a darkmode class to the body
+	//------------------------------------
+	$("#darkmode").click(function(){
+		// console.log('darkmode button clicked');
+		$('body').toggleClass('darkmode');
+	});
+
+});
+	
+
