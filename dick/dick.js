a8c354fabfe5941f339e23c345eb0f2160967404
yulebesorry <58015079+yulebesorry@users.noreply.github.com>
2021-04-16 16:26:37 -0700

    fetch

---
+++ dick/dick.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+$(document).ready(function(){
+
+
+	//------------------------------------
+	// adds a a darkmode class to the body
+	//------------------------------------
+	$("#darkmode").click(function(){
+		// console.log('darkmode button clicked');
+		$('body').toggleClass('darkmode');
+	});
+
+});
